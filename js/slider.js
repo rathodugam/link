48ed82eeddad1d17fe1d1953b67048777bc33d28
rathodugam <rathodugam1@gmail.com>
2022-12-13 13:03:51 +0530

    first commit

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.signin_slider', {
+  slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+  });
